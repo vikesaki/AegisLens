d1666987fc4567752464e23323bc790ad6f8941b
Maruf Habibie Siregar <maruf.habibie.siregar@gmail.com>
2025-07-17 11:06:47 +0700

    Update appProfile.py

--- Deployment/appProfile.py
+++ Deployment/appProfile.py
@@ -80,7 +80,6 @@ def app():
     )
 
     # TEAM PROFILE SECTION
-   def app():
     st.markdown("<h1 style='font-size: 48px; text-align: center;'>MEET OUR TEAM</h1>", unsafe_allow_html=True)
 
    
