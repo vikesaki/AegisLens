f97ab91595ccd6ddc6e2f2bbf4aced062f21945e
Maruf Habibie Siregar <maruf.habibie.siregar@gmail.com>
2025-07-17 11:14:48 +0700

    Update appProfile.py

--- Deployment/appProfile.py
+++ Deployment/appProfile.py
@@ -82,7 +82,7 @@ def app():
     # TEAM PROFILE SECTION
     st.markdown("<h1 style='font-size: 48px;text-align: center;'>MEET OUR TEAM</h1>", unsafe_allow_html=True)
     # Use columns with equal spacing for perfect centering
-    col1, col2, col3 = st.columns([1, 0.4, 1])
+    col1, col2, col3 = st.columns([1, 0.38, 1])
     with col2:
         st.image("Deployment/FotoKakLis.jpg", width=200)
         st.write("<p style='text-align: center;'>Lis Wahyuni</p>", unsafe_allow_html=True)
